03e963601162d0954640bf5d107f223308ffd95f
Nikita K <mendor@yuuzukiyo.net>
2021-01-29 15:42:19 +0200

    vcpkg manifests support

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
-int
-main() {
-    std::cout
-            << "Hello, World!"
-            << std::endl;
+#include <GLFW/glfw3.h>
+
+const unsigned int SCR_WIDTH  = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);               // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hazard", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to open GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window); // Initialize GLEW
+
+    glfwTerminate();
+
     return 0;
 }
